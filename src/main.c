6f05f64a61c0657dbdbb2d5dd8392741c100a684
Stalker2973 <omelete@mail.ru>
2018-05-02 15:29:33 +0700

    Add output of names of all files in the current directory

--- src/main.c
+++ src/main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+
+
 
 int main()
 {
-	
+	int count = 0;
+	DIR *dir;
+	DIR *nes_dir;
+	struct dirent *ent;
+
+	char directory[255] = "./";
+	char nested_dir[255];//Для вложенных директориях
+	dir = opendir(directory);
+	while ((ent = readdir(dir)) != NULL) {
+		if ((strcmp(ent->d_name, ".")) && (strcmp(ent->d_name, ".."))){
+			printf("%s\n", ent->d_name);
+			for (int i = 0; i < strlen(ent->d_name); ++i){
+				if (ent->d_name[i] == '.') {
+					count++;
+					//printf("ПРОВРЕКА 1 ---- %d\n", count);
+				}
+			}
+			if ((count == 0) && (strcmp(ent->d_name, "main"))) {
+				strcpy(nested_dir, ent->d_name);
+				//printf("ПРОВРЕКА 2 ---- %s\n", nested_dir);				
+			}
+			count = 0;			
+		}
+	}
+	closedir(dir);
+
+
 	return 0;
 }
