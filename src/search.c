e3f21724483eaef00255d012248e4a2a88ad00a9
SetLN10 <setln10@mail.ru>
2018-05-27 12:37:48 +0700

    Optimization of search algorithm

--- src/search.c
+++ src/search.c
@@ -10,6 +10,7 @@ IntVector *search(char *text, char *sample)
 	unsigned int text_length = strlen(text);
 	unsigned int sample_length = strlen(sample);
 
+	//Создаем вектор для записи индексов начал совпадающих подстрок
 	IntVector *result = int_vector_new(1);
 
 	//Если длина образца больше длины текста, то совпадений быть не может
@@ -17,26 +18,21 @@ IntVector *search(char *text, char *sample)
 		return result;
 	}
 
-	int br = 0;
-
 	//Пеебираем все возможные подстроки, выполняя сдвиг на один символ
 	for (unsigned int i = 0; i <= (text_length - sample_length); ++i) {
 		//Выполняем сравнение посимвольно
-		for (unsigned int j = 0; j < sample_length; ++j) {
+		unsigned int j;
+		for (j = 0; j < sample_length; ++j) {
 			//Если символы не совпадают, прерываем цикл
 			if (sample[j] != text[i + j]) {
-				br = 1;
 				break;
 			}
 		}
 
 		//Если совпадение найдено (цикл не был прерван)
-		if (!br) {
+		if (j == sample_length) {
 			//Добавляем индекс начала подстроки
 			int_vector_push_back(result, i);
-		} else {
-			//Иначе сбрасываем счетчик прерываний
-			br = 0;
 		}
 	}
 
