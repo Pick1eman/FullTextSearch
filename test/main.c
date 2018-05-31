f14bc9e92ce9069f790ce4e91d75a0c01e1471c8
Stalker2973 <omelete@mail.ru>
2018-05-31 18:17:05 +0700

    Add 1st test on search

--- test/main.c
+++ test/main.c
@@ -17,6 +17,52 @@ int r = 0, h = 0;
 extern size_t general_counter;
 
 
+
+
+CTEST(serach1, correct)
+{
+	IntVector *result = search("Hello world!", "world");
+	unsigned int expected = 6;
+	size_t expected1 = 1;
+	ASSERT_EQUAL_U(expected,result->arr[0]);
+	ASSERT_EQUAL_U(expected1,result->size);
+	ASSERT_EQUAL_U(expected1,result->capacity);
+	int_vector_free(result);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main(int argc, const char** argv)
 {
 	int test_result = ctest_main(argc, argv);
