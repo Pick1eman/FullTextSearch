2ba6469b382d44fc7f7952b9c9d9908d7bafba53
Stalker2973 <omelete@mail.ru>
2018-05-31 18:59:01 +0700

    Add test on string_tolower

--- test/main.c
+++ test/main.c
@@ -43,7 +43,6 @@ CTEST(serach2, correct)
 	int_vector_free(result);
 }
 
-
 CTEST(serach3, correct)
 {
 	IntVector *result = search("Kill gunslinger\n", "Ki");
@@ -66,13 +65,31 @@ CTEST(serach4, correct)
 }
 
 
+CTEST(serach5, correct)
+{
+	IntVector *result = search("Yuri\nSemetsky\n", "i\nS");
+	unsigned int expected = 3;
+	size_t expected1 = 1;
+	ASSERT_EQUAL_U(expected,result->arr[0]);
+	ASSERT_EQUAL_U(expected1,result->size);
+	ASSERT_EQUAL_U(expected1,result->capacity);
+	int_vector_free(result);
+}
 
 
 
 
-
-
-
+CTEST(string_tolower1, correct)
+{
+	char *result = (char*)malloc(24 * sizeof(char));
+	strcpy(result, "NLC 7:i'm Markered One\n");
+	result = string_tolower(result);
+	char *expected = (char*)malloc(24 * sizeof(char));
+	strcpy(expected, "nlc 7:i'm markered one\n");
+	ASSERT_STR(expected, result);
+	free(expected);
+	free(result);
+}
 
 
 
